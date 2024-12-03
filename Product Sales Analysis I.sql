ea253cb9aeb01dec487cb7fbc5ccae2a05b80c9e
reimauk <evgeniireimauk@gmail.com>
2024-12-03 20:41:30 +0200

    Add files via upload

---
+++ Product Sales Analysis I.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT
+    product_name,
+    year,
+    price
+FROM
+    Sales S
+    JOIN
+        Product P
+    ON
+        S.product_id = P.product_id
+WHERE
+    sale_id
