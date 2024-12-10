d72074e1723664724986ef2cb9da9ead819c6343
reimauk <evgeniireimauk@gmail.com>
2024-12-10 16:46:08 +0200

    Add files via upload

---
+++ Classes More Than 5 Students.sql
@@ -0,0 +1,69 @@
+Table: Courses
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+ 
+
+Write a solution to find all the classes that have at least five students.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+Explanation: 
+- Math has 6 students, so we include it.
+- English has 1 student, so we do not include it.
+- Biology has 1 student, so we do not include it.
+- Computer has 1 student, so we do not include it.
+
+-- Option #1
+# Write your MySQL query statement below
+SELECT
+    DISTINCT C2.class
+FROM
+    Courses C1
+    LEFT JOIN
+        (SELECT
+            class,
+            COUNT(class)
+        FROM
+            Courses
+        GROUP BY
+            class
+            HAVING COUNT(class) >= 5
+        ) AS C2
+    ON C1.class = C2.class
+WHERE C2.class IS NOT NULL;
+
+
